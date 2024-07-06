e50fe4ba080f825cbbc4e9eea516be94f255ae34
khemaraj003 <131021470+khemaraj003@users.noreply.github.com>
2024-07-06 11:57:31 +0530

    Add files via upload

---
+++ print_traingle_pattern.py
@@ -0,0 +1,22 @@
+def lower_triangular(n):
+    for i in range(1, n + 1):
+        print('* ' * i)
+
+def upper_triangular(n):
+    for i in range(n, 0, -1):
+        print('* ' * i)
+
+def pyramid(n):
+    for i in range(1, n + 1):
+        print(' ' * (n - i) + '* ' * i)
+
+# Example usage
+n = 5
+print("Lower Triangular Pattern:")
+lower_triangular(n)
+
+print("\nUpper Triangular Pattern:")
+upper_triangular(n)
+
+print("\nPyramid Pattern:")
+pyramid(n)
